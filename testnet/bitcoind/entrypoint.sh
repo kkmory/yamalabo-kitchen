1454da57248042fba1331f6d7e5b67dda4758abc
Keisuke Mori <rjfs29kei@gmail.com>
2019-10-01 19:10:55 +0900

    add volume setting for docker-compose, change config for testnet

--- testnet/bitcoind/entrypoint.sh
+++ testnet/bitcoind/entrypoint.sh
@@ -2,17 +2,15 @@
 
 set -euo pipefail
 
-# If config doesn't exist, initialize with sane defaults for running a
-# non-mining node.
-
 if [ ! -e "${BITCOIN_CONF}" ]; then
   tee -a >${BITCOIN_CONF} <<EOF
+testnet=3
 txindex=1  
 server=1   
 rest=1      
 rpcuser=${BITCOIN_RPCUSER}
 rpcpassword=${BITCOIN_RPCPASSWORD}
-rpcport=18332 
+rpcport=18332
 EOF
 fi
 

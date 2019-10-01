039768671aa567803fe33e907623ca91a85c8cb5
Keisuke Mori <rjfs29kei@gmail.com>
2019-10-02 02:42:22 +0900

    impl done bitcoin-testnet-node

--- testnet/bitcoind/entrypoint.sh
+++ testnet/bitcoind/entrypoint.sh
@@ -2,17 +2,15 @@
 
 set -euo pipefail
 
-if [ ! -e "${BITCOIN_CONF}" ]; then
-  tee -a >${BITCOIN_CONF} <<EOF
+cat <<EOF > ${BITCOIN_CONF}
 testnet=3
 txindex=1  
 server=1   
 rest=1      
 rpcuser=${BITCOIN_RPCUSER}
-rpcpassword=${BITCOIN_RPCPASSWORD}
+rpcpassword=${BITCOIN_RPCPASS}
 rpcport=18332
 EOF
-fi
 
 if [ $# -eq 0 ]; then
   exec bitcoind -datadir=${BITCOIN_DIR} -conf=${BITCOIN_CONF}

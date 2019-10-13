a3661134ef08d29dda254389cb5ad40c21e05f71
Keisuke Mori <rjfs29kei@gmail.com>
2019-10-13 13:14:51 +0900

    wip : edit capistrano setting for production

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,37 +3,31 @@ lock "~> 3.11.1"
 
 set :application, "yamalabo-nodes"
 set :repo_url, "git@github.com:moririn772/univ-bcnodes.git"
+set :pty, true 
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
+# デプロイ先の設定
 set :deploy_to, "/usr/yamalabo/src"
+role :test_btc, ENV['YAMALABO_UBUNTU_HOSTS'].split(",")
+role :test_host, "localhost"
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
+set :ssh_options, {
+  user: ENV['YAMALABO_UBUNTU_USER'],
+  password: ENV['YAMALABO_UBUNTU_PASS'],
+  auth_methods: %w(password),
+  forward_agent: false,
+}
 
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+desc "デプロイ"
+task :deploy do 
+  on roles(:nuc) do
+    deploy_to = fetch :nuc
+    execute "echo complete deploy!!!!"
+  end
+end
 
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
-
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
+desc "テスト"
+task :test do
+  on roles(:test_host) do
+    execute "pwd"
+  end
+end
